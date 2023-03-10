75440286e2848536e316fb6d608d2e51198e8091
pabloc <programacion7@hilagro.com.py>
2023-03-10 12:34:19 -0300

    formato factura SET

---
+++ PLSQL/formato_nro_documento_set.sql
@@ -0,0 +1,10 @@
+select 
+      -- 10/03/2023 12:33 @PabloACespedes
+      -- tener en cuenta que l_nro_documento debe ser hasta 13 digitos maximos, y como minimo 10 digitos
+      lpad( substr( l_nro_documento , 0, length(l_nro_documento)-10 ), 3, 0 )
+      || '-' ||
+      substr(l_nro_documento, -10, 3)
+      || '-' ||
+      substr(l_nro_documento, -7, 7)
+    x  
+from dual;
