197ac7ac58947d14212acc38646ad9b8f02ba561
pabloc <programacion7@hilagro.com.py>
2023-03-10 12:35:28 -0300

    ajuste de comentario

--- PLSQL/formato_nro_documento_set.sql
+++ PLSQL/formato_nro_documento_set.sql
@@ -1,6 +1,7 @@
 select 
       -- 10/03/2023 12:33 @PabloACespedes
       -- tener en cuenta que l_nro_documento debe ser hasta 13 digitos maximos, y como minimo 10 digitos
+      -- 001 001 0000001
       lpad( substr( l_nro_documento , 0, length(l_nro_documento)-10 ), 3, 0 )
       || '-' ||
       substr(l_nro_documento, -10, 3)
