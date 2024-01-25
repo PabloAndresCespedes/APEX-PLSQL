088281797ffcf82b91c07264c0a745822a295b66
pabloc <programacion7@hilagro.com.py>
2024-01-25 17:09:35 -0300

    ajustes varios para regex

--- PLSQL/extraer_solo_numero_o_char_regex.sql
+++ PLSQL/extraer_solo_numero_o_char_regex.sql
@@ -11,3 +11,10 @@ select regexp_replace('stack12345overflow569', '[[:alpha:]]|_') as numbers from 
 select regexp_replace('stack12345overflow569', '[^0-9]', '') as numbers,
        regexp_replace('Stack12345OverFlow569', '[^a-z and ^A-Z]', '') as characters
 from dual
+
+/*
+ * Para eliminado de espacios
+ Ref http://www.tecnoent.com/1366-reemplazar-multiples-espacios-oracle
+*/
+
+substr(regexp_replace(regexp_replace(p.prov_razon_social, '^a-z and ^A-Z and [^0-9]', ''), '  *', ' '), 0, 50)
