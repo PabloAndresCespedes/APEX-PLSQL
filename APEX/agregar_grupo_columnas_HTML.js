9ff2268749172c64a48b05db02dd241dfa783109
pabloc <programacion7@hilagro.com.py>
2024-02-15 16:57:51 -0300

    ajsutes metodo para insertar grupo de columnas

--- APEX/agregar_grupo_columnas_HTML.js
+++ APEX/agregar_grupo_columnas_HTML.js
@@ -25,3 +25,22 @@ $(this.affectedElements).html(function(index,currentcontent){
 
 // short sentence
 $(this.affectedElements).html((index,currentcontent) => templateHeader() + currentcontent);
+
+
+/// VERSION 2
+const REG_DET_SOL_LISTA = 'staticIdRegion';
+
+function templateHeader(){
+    const templ = `<tr>
+                        <th class="t-Report-colHead" align="center" colspan="4">Finanzas<br><small>Registro(s) actualmente en finanzas ya incluyendo redefiniciones.</small></th>
+                        <th class="t-Report-colHead" align="center" colspan="2">Al Registrar<br><small>Registro(s) al realizarse la solicitud.</small></th>
+                        <th class="t-Report-colHead" align="center" colspan="1">Pagos<br><small>Pagos de la cuota.</small></th>
+                        <th class="t-Report-colHead" align="center" colspan="1">Saldos<br><small>Diferencia a pagar de la cuota en finanzas.</small></th>
+                    </tr>`;
+
+    return templ;
+}
+/***********************************************************/
+apex.jQuery( '#' + REG_DET_SOL_LISTA ).on( "apexafterrefresh", function() {
+    $('#report_table_' + REG_DET_SOL_LISTA + ' > thead').html((index,currentcontent) => templateHeader() + currentcontent);
+});
