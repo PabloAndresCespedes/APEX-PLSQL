31c4bc4b77620448a43340a14fb181ce60199aa5
pabloc <programacion7@hilagro.com.py>
2024-04-08 10:33:31 -0400

    ajustes para logica de manejo de errores en APEX, unificar proceso para exportar e importar los archivos

--- APEX/manejoErrores.js
+++ APEX/manejoErrores.js
@@ -1,60 +1,61 @@
 // Author: @PabloACespedes
 // Manejo de errores en oracle APEX JS
 // Documentation link: https://docs.oracle.com/en/database/oracle/application-express/20.1/aexjs/apex.message.html#.showErrors
-
 var error = {};
 
 /**
  * @item {string} item - Nombre del item
- * @mensaje {string} mensaje - Mensaje de error
+ * @msg {string} msg - msg de error
  */
-error.item = function ({item, mensaje}) {
+error.item = function ({item, msg}) {
     apex.message.clearErrors();
 
     apex.message.showErrors({
         type: "error",
-        location: ["item"],
+        location: "inline",
         pageItem: item,
-        message: mensaje,
-        unsafe: false
+        message: msg,
+        unsafe: true
     });
-
 }
 
 /*
-* @mensaje {string} mensaje - Mensaje de error
+* @msg {string} msg - msg de error
   Se utiliza a nivel de pagina
 */
-error.page = function ({mensaje}) {
+error.page = function ({msg}) {
     apex.message.clearErrors();
 
     apex.message.showErrors({
         type: "error",
-        location: ["page"],
-        message: mensaje,
-        unsafe: false
+        location: "page",
+        message: msg,
+        unsafe: true
     });
 }
 
 /**
  * 
  * @param items {Array} - Array de objetos con los siguientes atributos
- * Estructura {itemName: "P1_ITEM", mensajeError: "Complete este campo"},
- *            {itemName: "P2_ITEM", mensajeError: "Complete este campo"},     
+ * Estructura {itemName: "P1_ITEM", msg: "Complete este campo"},
+ *            {itemName: "P2_ITEM", msg: "Complete este campo"},     
  */
 error.items = function (items){
   apex.message.clearErrors();
 
   let errors = []; 
 
-  for ( const item in items){
+  for ( const item in items ){
     errors.push({
         type:       "error",
-        location:   [ "inline" ],
+        location:   "inline",
         pageItem:   items[item].itemName,
-        message:    items[item].mensajeError,
-        unsafe:     false
+        message:    items[item].msg,
+        unsafe:     true
     });
   }
+
   apex.message.showErrors(errors);
 }
+
+export { error };
