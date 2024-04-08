006b51349eb0e9af5de32d79dfe24c524e349b47
pabloc <programacion7@hilagro.com.py>
2024-04-08 07:19:17 -0400

    seguimiento para libro, pruebas de creación de metodos en js

--- APEX/eloquent.js
+++ APEX/eloquent.js
@@ -1,5 +1,25 @@
-function sumar(a, b) {
-    return a + b;
+// Chapter 6: Eloquent JavaScript
+// La vida secreta de los objetos
+// Methods
+console.log('====================================================');
+let conejo = {ciudad: "C9"};
+
+conejo.hablar = function(line) {
+  console.log(`El conejo dice '${line}' de la ciudad de ${this.ciudad}`);
 }
 
-console.log(sumar(2, 3)); //
+conejo.hablar("Estoy vivo"); // → El conejo dice 'Estoy vivo'
+
+function hablar (line) {
+  console.log(`El ${this.tipo} conejo dice '${line}'`);
+}
+
+let conejoBlanco = {tipo: "blanco", hablar};
+let conejoHambriento = {tipo: "hambriento", hablar};
+
+console.log('====================================================');
+conejoBlanco.hablar("Soy un conejo blanco"); // → El blanco conejo dice 'Soy un conejo blanco'
+conejoHambriento.hablar("Estoy hambriento"); // → El hambriento conejo dice 'Estoy hambriento'
+console.log('====================================================');
+
+
