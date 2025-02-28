bbd93794b4ecadd3c983c9075c87bae6a7f0b866
@PabloACespedes <programacion7@hilagro.com.py>
2025-02-28 17:10:14 -0300

    ajuste para archivo

--- otros/NodeJSMarcaciones/index.js
+++ otros/NodeJSMarcaciones/index.js
@@ -28,51 +28,52 @@ const l_doc_ci = 5253581; // Número de cedula del colaborador
     // Referencia de horarios en https://crontab.guru/
     const HS_MARCACION = {
         DOMINGO: {
-            DIURNO_ENTRADA: '00 6 * * *', // 06:00
-            DIURNO_SALIDA: '00 12 * * * ', // 12:00
-            VESPERTINO_ENTRADA: '00 13 * * *', // 13:00
-            VESPERTINO_SALIDA: '00 17 * * * ', // 17:00
+            DIURNO_ENTRADA     : '00 6 * * *'   , // 06:00
+            DIURNO_SALIDA      : '00 12 * * * ' , // 12:00
+            VESPERTINO_ENTRADA : '00 13 * * *'  , // 13:00
+            VESPERTINO_SALIDA  : '00 17 * * * ' , // 17:00
         },
         LUNES: {
-            DIURNO_ENTRADA: '53 6 * * *', // 06:53
-            DIURNO_SALIDA: '01 12 * * * ', // 12:01
-            VESPERTINO_ENTRADA: '57 12 * * *', // 12:57
-            VESPERTINO_SALIDA: '02 17 * * * ', // 17:02
+            DIURNO_ENTRADA     : '53 6 * * *'   , // 06:53
+            DIURNO_SALIDA      : '01 12 * * * ' , // 12:01
+            VESPERTINO_ENTRADA : '57 12 * * *'  , // 12:57
+            VESPERTINO_SALIDA  : '02 17 * * * ' , // 17:02
         },
         MARTES: {
-            DIURNO_ENTRADA: '55 6 * * *', // 06:55
-            DIURNO_SALIDA: '03 12 * * *', // 12:03
-            VESPERTINO_ENTRADA: '52 12 * * *', // 12:52
-            VESPERTINO_SALIDA: '01 17 * * *', // 17:01
+            DIURNO_ENTRADA     : '55 6 * * *'  , // 06:55
+            DIURNO_SALIDA      : '03 12 * * *' , // 12:03
+            VESPERTINO_ENTRADA : '52 12 * * *' , // 12:52
+            VESPERTINO_SALIDA  : '01 17 * * *' , // 17:01
         },
         MIERCOLES: {
-            DIURNO_ENTRADA: '59 6 * * *', // 06:59
-            DIURNO_SALIDA: '05 12 * * *', // 12:05
-            VESPERTINO_ENTRADA: '57 12 * * *', // 12:57
-            VESPERTINO_SALIDA: '15 17 * * *', // 17:15
+            DIURNO_ENTRADA     : '59 6 * * *'  , // 06:59
+            DIURNO_SALIDA      : '05 12 * * *' , // 12:05
+            VESPERTINO_ENTRADA : '57 12 * * *' , // 12:57
+            VESPERTINO_SALIDA  : '15 17 * * *' , // 17:15
         },
         JUEVES: {
-            DIURNO_ENTRADA: '55 6 * * *', // 06:55
-            DIURNO_SALIDA: '03 12 * * *', // 12:03
-            VESPERTINO_ENTRADA: '52 12 * * *', // 12:52
-            VESPERTINO_SALIDA: '01 17 * * *', // 17:01
+            DIURNO_ENTRADA     : '55 6 * * *'  , // 06:55
+            DIURNO_SALIDA      : '03 12 * * *' , // 12:03
+            VESPERTINO_ENTRADA : '52 12 * * *' , // 12:52
+            VESPERTINO_SALIDA  : '01 17 * * *' , // 17:01
         },
         VIERNES: {
-            DIURNO_ENTRADA: '55 6 * * *', // 06:55
-            DIURNO_SALIDA: '03 12 * * *', // 12:03
-            VESPERTINO_ENTRADA: '55 12 * * *', // 12:55
-            VESPERTINO_SALIDA: '03 17 * * *', // 17:03
+            DIURNO_ENTRADA     : '55 6 * * *'  , // 06:55
+            DIURNO_SALIDA      : '03 12 * * *' , // 12:03
+            VESPERTINO_ENTRADA : '55 12 * * *' , // 12:55
+            VESPERTINO_SALIDA  : '01 17 * * *' , // 17:03
         },
         SABADO: {
-            DIURNO_ENTRADA: '59 6 * * *', // 06:59
-            DIURNO_SALIDA: '07 12 * * *', // 12:07
-            VESPERTINO_ENTRADA: '52 12 * * *', // 12:52
-            VESPERTINO_SALIDA: '00 17 * * *', // 17:00
+            DIURNO_ENTRADA     : '59 6 * * *'  , // 06:59
+            DIURNO_SALIDA      : '07 12 * * *' , // 12:07
+            VESPERTINO_ENTRADA : '52 12 * * *' , // 12:52
+            VESPERTINO_SALIDA  : '00 17 * * *' , // 17:00
         }
   };
 //==============================================================================================================
+    const diasSemana = ['DOMINGO', 'LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES', 'SABADO'];   
+
     function obtenerMarcacionHoy(marcacion) {
-        const diasSemana = ['DOMINGO', 'LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES', 'SABADO'];
         const hoy = new Date();
         const nombreDia = diasSemana[hoy.getDay()];
 
@@ -83,7 +84,8 @@ const l_doc_ci = 5253581; // Número de cedula del colaborador
     
     const getRandomInt = (min, max) => {
         return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
+    }
+
     function generarSegundosAleatorios() {
         return getRandomInt(1, 11) * 1000; // Convertir segundos a milisegundos
     }
@@ -115,6 +117,18 @@ const l_doc_ci = 5253581; // Número de cedula del colaborador
         setTimeout(() => {
             realizarPost("S");
         }, generarSegundosAleatorios());
+    });
+//==============================================================================================================
+    app.get('/', (req, res) => {
+        let l_current = new Date(); // (0) domingo (6) sabado
+        let l_day   = l_current.getDay();
+
+        res.send(`Este script ejecuta la marcación en Consultagro de forma programada. 
+                  Hoy será ejecutado todo lo referente 
+                  al día: ${ diasSemana[l_day] }
+                `
+        );
+
     });
 //==============================================================================================================
     app.listen(port, () => {
