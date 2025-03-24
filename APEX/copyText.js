d1b810c9b450f2f598f051b171f6b2f20183c75d
@PabloACespedes <programacion7@hilagro.com.py>
2025-03-24 08:39:53 -0300

    Cambios varios se agrega alternativa para copiado desde el Browser

--- APEX/copyText.js
+++ APEX/copyText.js
@@ -2,10 +2,17 @@
 // Para esto se necesita HTTPS 
 // o activar el dominio en chrome://flags/#unsafely-treat-insecure-origin-as-secure
 // agregar el dominio http://localhost:8080
-async function copyText(text){
-    navigator.clipboard.writeText(text).then(() => {
-      apex.message.showPageSuccess('Texto copiado correctamente: ' + text);
-    },() => {
-      console.error('Failed to copy');
-    });
+async function writeClipboardText(text) {
+  try {
+      await navigator.clipboard.writeText(text);
+  } catch (error) {
+      console.error(error.message);
+  }
 }
+
+
+// Utilizacion llamar accion dinamica con el siguiente codigo
+// Esto obtiene el valor de un item y lo envia a la funcion
+let l_contenido = apex.item('P777_RESULT').getValue();
+
+writeClipboardText(l_contenido).then(() => {apex.message.showPageSuccess( "Copiado en cortapapeles!" );});
